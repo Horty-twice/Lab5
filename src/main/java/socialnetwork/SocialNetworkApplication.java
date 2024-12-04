7022bd26eddeae6b3170e9589674aab94e309ed9
Horty_twice <shahyginsasha5@gmail.com>
2024-12-04 22:19:01 +0300

    Test1

--- src/main/java/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/socialnetwork/SocialNetworkApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SocialNetworkApplication {
+public class SocialNetworkApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SocialNetworkApplication.class, args);
